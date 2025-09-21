e1d940ce6b55b2914db32f4dad8e6c996da3c235
ngquanghuy <ngquanghuy012@gmail.com>
2025-09-21 09:03:51 +0700

    Upload

--- Type_Erasu/function.cpp
+++ Type_Erasu/function.cpp
@@ -7,6 +7,10 @@ function<int(int, int)> operation;
 int multiply(int a, int b) {
     return a * b;
 }
+
+auto chia(int a, int b) {
+    return a / b;
+}
 int main() {
     operation = [](int a, int b) {
         return a + b;
@@ -14,5 +18,7 @@ int main() {
     cout << "Add: " << operation(50, 60) << endl;
     operation = multiply;
     cout << "Multiply: " << operation(50, 70) << endl;
+    operation = chia;
+    cout << "END: " << operation(100, 2) << endl;
     return 0;
 }
